4362795547067b4776828a8238983002ef78dd57
rabiks <sr@2rsystems.pl>
2023-05-31 18:55:30 +0200

    fixes

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('srabikowski/50299/people', 'App\Http\Controllers\PeopleController@index');
+Route::get('srabikowski/50299/people/{id}', 'App\Http\Controllers\PeopleController@show');
+Route::put('srabikowski/50299/people/{id}', 'App\Http\Controllers\PeopleController@update');
+Route::delete('srabikowski/50299/people/{id}', 'App\Http\Controllers\PeopleController@destroy');
+Route::post('srabikowski/50299/people', 'App\Http\Controllers\PeopleController@store');
 
 Route::get('/', function () {
     return view('welcome');
